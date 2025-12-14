4d5b63209429b9f41fab5774fe866d8828064144
Tanaphat Saeung <tanaphat.sa2@gmail.com>
2025-12-14 19:12:48 +0700

    finish cookie

--- 2-NodeJs/8Express&Cookie/index.js
+++ 2-NodeJs/8Express&Cookie/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser('thisismysecret')) // signed-cookie
+
 // const shelterRoutes = require('./routes/shelters')
 // const dogRoutes = require('./routes/dogs')
 // const adminRoute = require('./routes/admin')
@@ -9,13 +14,25 @@ const app = express()
 // app.use('/admin',adminRoute )
 
 app.get('/greet', (req,res)=>{
-    res.send('Hey there')
+    const { name = 'anonymous' } = req.cookies
+    res.send(`Hey there, ${name}`)
 })
 
 app.get('/setName',(req,res)=>{
     res.cookie('name','gusty')
     res.cookie('animal','harlequin shrimp')
     res.send('Sent your cookie!')
+})
+
+app.get('/getsignedcookies', (req,res)=>{
+    res.cookie('fruit','grape', { signed: true })
+    res.send('SIGNED YOUR COOKIE ALREADY')
+})
+
+
+app.get('/verifyfruit', (req,res)=>{
+    const { fruit } = req.signedCookies
+    res.send(fruit)
 })
 
 app.listen(3000, () => {
